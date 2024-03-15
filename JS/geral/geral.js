cbedcf5effed829e190d82a8e8028d65fcf648e6
Victor Fernandes Neves da Silva <108033088+victortxt@users.noreply.github.com>
2024-03-15 11:29:46 -0300

    commit

--- JS/geral/geral.js
+++ JS/geral/geral.js
@@ -2,12 +2,20 @@
 
 const menu = document.querySelector('#menuBtn');
 let navbar = document.querySelector('#navBar')
+const closeMenu = document.querySelector('#closeMenu')
 let id = 0
 menu.addEventListener('click', () => {
     if(id === 0){
         document.querySelector('nav').style.display = 'block'
         id++
     }else{
+        document.querySelector('nav').style.display = 'none'
+        id--
+    }
+})
+
+closeMenu.addEventListener('click', () => {
+    if(id != 0){
         document.querySelector('nav').style.display = 'none'
         id--
     }
