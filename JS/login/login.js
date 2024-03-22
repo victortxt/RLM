3bd1043d4aeb94e9c77a6ca4a0243edf1b97057a
Victor Fernandes Neves da Silva <108033088+victortxt@users.noreply.github.com>
2024-03-22 09:42:56 -0300

    commit

--- JS/login/login.js
+++ JS/login/login.js
@@ -1,90 +1,30 @@
-const buttonsOptMobile = document.querySelectorAll('.btn');
 const buttonLog = document.querySelector('#login');
 const buttonCreate = document.querySelector('#create');
-const inputs = document.querySelectorAll('.inputContent');
-const submitCreate = document.querySelector('#createSub');
-const submitLog = document.querySelector('#loginSub');
-let typeText = 'cadastro'
-let objDatas = {
-    name: '',
-    password: '',
-    email: '',
-}
-
-submitCreate.addEventListener('click', (element) => {
-    submitForm(element)
-})
-
-submitLog.addEventListener('click', (element) => {
-    submitForm(element)
-})
+let form = document.querySelector('form')
+let urlForm = ''
 
 
-function submitForm(element) {
-    element.preventDefault();
-    let arrayTemp = Array()
-    inputs.forEach((e) => {
-        arrayTemp.push(e.value);
-    })
-
-    objDatas.name = arrayTemp[0]
-    objDatas.password = arrayTemp[1]
-    objDatas.email = arrayTemp[2]
-
-    for(let i in objDatas){
-        if(objDatas[i] === ''){
-            alert(`Não foi possível realizar o ${typeText}`);
-            break
-        }else{
-            //aplicar o method AJAX;
-        }
-    }
-}
-
-buttonsOptMobile.forEach(element => {
-    element.addEventListener('click', (e) => {
-        if(e.target.className.indexOf('defaultControl') != -1){
-            document.querySelector('.formEmpresa').style.display = 'none';
-            objDatas = {
-                name: '',
-                password: '',
-                email: '',
-            }
-        }else{
-            document.querySelector('.formEmpresa').style.display = 'block';
-            objDatas = {
-                name: '',
-                password: '',
-                email: '',
-                material:'' 
-            }
-        }
-
-    })
-});
-
-//create or log
 buttonLog.addEventListener('click', () => {
-    typeText = 'login'
     document.querySelector('#containerCreateAcount').style.display = 'none'
     document.querySelector('#containerLoginAcount').style.display = 'block'
-    document.querySelector('.formEmpresa').style.display = 'none';
     document.querySelector('#emailBoxText').style.display = 'none';
-    objDatas = {
-        name: '',
-        password: '',
-    }
-    
+    document.querySelector('.buttonsLine').style.display = 'none';
+    document.querySelector('.btnLineDesk').style.display = 'none';
+    document.querySelector('.noneTextBasic').textContent = 'Área de login'
+    document.querySelector('.textIndentify').style.display = 'none';
+    urlForm = 'a'
+    form.action = urlForm
 })
 
 buttonCreate.addEventListener('click', () => {
-    typeText = 'cadastro'
     document.querySelector('#containerCreateAcount').style.display = 'block'
     document.querySelector('#containerLoginAcount').style.display = 'none'
     document.querySelector('#emailBoxText').style.display = 'flex';
-    objDatas = {
-        name: '',
-        password: '',
-        email: '',
-    }
+    document.querySelector('.buttonsLine').style.display = 'flex'
+    document.querySelector('.btnLineDesk').style.display = 'flex';
+    document.querySelector('.noneTextBasic').textContent = 'Selecione quem é você';
+    document.querySelector('.textIndentify').style.display = 'block';
+    urlForm = 'b'
+    form.action = urlForm
+
 })
