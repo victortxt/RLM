278a0b6db10591e946275148f09f254b5e46f75a
Victor Fernandes Neves da Silva <108033088+victortxt@users.noreply.github.com>
2024-04-09 12:33:12 -0300

    commit

--- JS/sobre/sobre.js
+++ JS/sobre/sobre.js
@@ -1,14 +1,45 @@
-/*
-btnLeft
-btnRight
-*/
+const slideSquare2 = document.querySelector('.img2sobre')
+const slideSquare1 = document.querySelector('.img1sobre')
+
+let idSlide = 0
+let urlImage = {
+    url1: '',
+    url2: ''
+}
+
+function typeOfImgSlide(condition){
+    switch (condition) {
+        case 0:
+            urlImage.url1 = '../../img/IMG_7485.JPG'
+            urlImage.url2 = '../../img/IMG_7659.jpg'
+            break
+        case 1: 
+            urlImage.url1 = '../../img/IMG_7498.JPG'
+            urlImage.url2 = '../../patrick'
+            break
+        case 2:
+            urlImage.url1 = '../../img/IMG_7661.jpg'
+            urlImage.url2 = '../../img/IMG_7658.JPG'
+            break
+    }
+
+    slideSquare2.style.backgroundImage = `url(${urlImage.url2})`
+    slideSquare1.style.backgroundImage = `url(${urlImage.url1})`
+}
 
 document.querySelector('#btnLeft').addEventListener('click', () => {
-    alert('esquerda')
+    if(idSlide > 0){
+        idSlide--
+        typeOfImgSlide(idSlide)
+    }
+    
 })
 
 document.querySelector('#btnRight').addEventListener('click', () => {
-    alert('direita')
+    if(idSlide < 2){
+        idSlide++
+        typeOfImgSlide(idSlide)
+    }
 })
 
 // Variáveis para controlar o ID e a posição anterior do scroll
