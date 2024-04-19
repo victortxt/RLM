05f4d51db4e90cf81cf4fdf2cc309d894879db2c
Victor Fernandes Neves da Silva <108033088+victortxt@users.noreply.github.com>
2024-04-19 09:46:02 -0300

    commit

--- JS/sobre/sobre.js
+++ JS/sobre/sobre.js
@@ -41,44 +41,20 @@ document.querySelector('#btnRight').addEventListener('click', () => {
         typeOfImgSlide(idSlide)
     }
 })
-
-// Variáveis para controlar o ID e a posição anterior do scroll
-let currentId = 60;
-let prevScrollPos = window.pageYOffset;
-
-// Função para atualizar o ID com base no movimento do scroll
 function updateIdOnScroll() {
-    const currentScrollPos = window.pageYOffset;
-    const scrollDirection = currentScrollPos > prevScrollPos ? 'down' : 'up';
+    const scrollPosition = window.scrollY;
+    console.log(scrollPosition)
 
-    if (scrollDirection === 'up') {
-        // Rolar para cima: aumentar o ID
-        if(currentId !== 60){
-            currentId = currentId + 20;
-        }
+    const divElement = document.querySelector("#Bg");
 
-        if(currentId === 60){
-            document.querySelector('.buttons-slide').style.display = 'flex'
-        }
-        
-    } else {
-        // Rolar para baixo: diminuir o ID
-        if(currentId !== 0 ){
-            currentId = currentId - 20;
-        }
-
-        document.querySelector('.buttons-slide').style.display = 'none'
+    if (scrollPosition < 30) {
+        $(divElement).fadeIn("slow");
+    }
+    else{
+        $(divElement).fadeOut("slow");
     }
 
-    // Atualizar a posição anterior do scroll
-    prevScrollPos = currentScrollPos;
-
-    // Exemplo de uso: imprime o ID atual no console
-    console.log(`ID atual: ${currentId}`);
-    document.querySelector('#Bg').style.height = currentId + 'vh'
 }
 
-// Adicione um listener para o evento de scroll
-window.addEventListener('scroll', updateIdOnScroll);
-
-
+// Call the function when the window is scrolled
+window.addEventListener("scroll", updateIdOnScroll);
